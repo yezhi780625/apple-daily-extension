d9224d16b01baf7874ae539ffacc0f2faebc0fd0
yezhi780625 <yezhi780625@gmail.com>
2019-04-12 18:48:58 +0800

    refactor showContents

--- scripts/showContents.js
+++ scripts/showContents.js
@@ -1,2 +1,11 @@
-Array.from(document.getElementsByClassName('ndArticle_content clearmen'))[0].children[0].style.display='block';
-Array.from(document.getElementsByClassName('ndPaywall'))[0].style.display='none';
+const content = Array.from(document.getElementsByClassName('ndArticle_content clearmen'))[0];
+const loginBlock = Array.from(document.getElementsByClassName('ndPaywall'))[0];
+
+
+if (content && content.children && content.children[0]) {
+  content.children[0].style.display='block'
+}
+
+if (loginBlock) {
+  loginBlock.style.display='none'
+}
