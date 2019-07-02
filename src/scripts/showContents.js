876445e1e2331fa53f4da2f002aa6e038cf6fee9
Vi Pro <VdustR@gmail.com>
2019-07-03 01:21:58 +0800

    fix: fix

--- src/scripts/showContents.js
+++ src/scripts/showContents.js
@@ -1,11 +1,12 @@
 $(async () => {
+  await (() => new Promise(resolve => setTimeout(resolve, 1000)))();
   const enabled = await getEnabled();
   if (!enabled) return;
   const body = await fetch(location);
   const html = await body.text();
   const $html = $(html);
   const $wrapper = $html.filter((i, el) => $(el).hasClass('wrapper'));
-
+  if ($wrapper.find('.ndArticle_contentBox').length === 0) return;
   const $scripts = $wrapper.find('script');
   $scripts.each((i, el) => {
     const $el = $(el);
@@ -13,13 +14,22 @@ $(async () => {
       $el
         .html()
         .replace(/document.write\(/g, '(() => {})(')
+        .replace(
+          `var effects =`,
+          `var effects = () => { $('.ndAritcle_headPic,.ndArticle_margin,.mediabox,.articulum').css('visibility','visible').show(); throw new Error('peace!'); }; false ===`
+        )
         .replace('switch(e){', 'switch("ad"){')
         .replace('OMOSDK.auth().getUserInfo().isLoggedIn', 'true')
-        .replace('omoUserType != "2"', 'true')
+        .replace('omoUserType != "2"', 'false')
         .replace('if(isUserEntitled){', 'if(true){')
+        .replace('(storageSupported && !localExpired)', '(false)')
         .replace(
-          /var ([A-Za-z0-9]+) = setInterval\(function\(\) {/g,
-          'clearInterval($1); var $1 = setInterval(function() { return;'
+          `if(typeof(paywall) == 'undefined' || typeof(OMOSDK) != 'object' || typeof(OMOSDK.auth) != 'function'){`,
+          'if (false){'
+        )
+        .replace(
+          `$('.ndgFixedtop').css({'height': topFixedheight + 'px'});`,
+          `$('.ndgFixedtop').css({'height': 0});`
         )
     );
   });
